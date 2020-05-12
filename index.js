6bf87310ef18276d5607ac3f2e0a7aadb83e9ae3
Daniel Son <dson1204@gmail.com>
2020-05-12 20:08:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string.toLowerCase) {
-    return "I can't hear you!";
+  switch (string) {
+  case string.toLowerCase:
+    return "I can\'t hear you!"
+  }
   }
   else if (string.toUpperCase) {
     return "YES INDEED!";
